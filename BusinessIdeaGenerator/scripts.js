ec533eb71eca62c76726e7257d58864dc6332f9f
Jesus Bejar <israelbejarii@gmail.com>
2025-02-15 13:47:16 -0700

    got elements and made business idea array

--- BusinessIdeaGenerator/scripts.js
+++ BusinessIdeaGenerator/scripts.js
@@ -1,6 +1,11 @@
 // get elements
+const generateBtn = document.querySelector("#generate-btn")
+const saveBtn = document.querySelector("#save-btn")
+const ideaDisplay = document.querySelector("#idea-display")
+const savedIdeasList = document.querySelector("#saved-ideas")
 
 // array with business ideas
+const businessIdeaArray = ["VC-backed landscaping marketplace", "Bootstrapped ancient text library"]
 
 // generate idea event
 

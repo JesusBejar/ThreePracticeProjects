3485f858c1886fb897d16e95bf58fdad4bb8427b
Jesus Bejar <israelbejarii@gmail.com>
2025-02-15 13:54:57 -0700

    added getRandomStr function and started event listener

--- BusinessIdeaGenerator/scripts.js
+++ BusinessIdeaGenerator/scripts.js
@@ -8,5 +8,19 @@ const savedIdeasList = document.querySelector("#saved-ideas")
 const businessIdeaArray = ["VC-backed landscaping marketplace", "Bootstrapped ancient text library"]
 
 // generate idea event
+generateBtn.addEventListener('click', e => {
+    let str = getRandomStr(businessIdeaArray)
+    console.log(str)
+
+})
 
 // save idea event
+
+// get random string from array to display
+function getRandomStr (array) {
+    if (array.length === 0) {
+        return undefined
+      }
+      const randomIndex = Math.floor(Math.random() * array.length);
+      return array[randomIndex]
+}

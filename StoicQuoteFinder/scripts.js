5939d37c590e35d8729fffe3a1dd481cf08a068a
Jesus Bejar <israelbejarii@gmail.com>
2025-02-17 10:52:07 -0700

    connected to quote API

--- StoicQuoteFinder/scripts.js
+++ StoicQuoteFinder/scripts.js
@@ -5,9 +5,19 @@ const quoteDisplay = document.querySelector("#quote-display")
 const savedQuotes = document.querySelector("#saved-quotes")
 
 // connect to API
+async function getAQuote() {
+    try {
+        // console.log('clicked')
+        const response = await fetch('https://stoic-quotes.com/api/quote')
+        // console.log(response)
+        const json = await response.json()
+        const quote = json.text
+        console.log(quote)
+    } catch (e) {
+        console.error(e)
+    }
+
+}
 
 // fetch quote event
-
-// save quote event
-
-// randomize function
+getBtn.addEventListener('click', getAQuote)

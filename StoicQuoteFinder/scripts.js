c4df38d2643fd5bc517ed67b2d6675abcf95efd2
Jesus Bejar <israelbejarii@gmail.com>
2025-02-17 10:13:21 -0700

    made quotes and got elements

---
+++ StoicQuoteFinder/scripts.js
@@ -0,0 +1,13 @@
+// get elements
+const getBtn = document.querySelector("#fetch-quote")
+const saveBtn = document.querySelector("#save-quote")
+const quoteDisplay = document.querySelector("#quote-display")
+const savedQuotes = document.querySelector("#saved-quotes")
+
+// connect to API
+
+// fetch quote event
+
+// save quote event
+
+// randomize function

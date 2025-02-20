64b6923df88f6ce1086a483cd9e3758f3c1e8f4e
Jesus Bejar <israelbejarii@gmail.com>
2025-02-19 20:00:32 -0700

    finished last project

--- MiniMarketplace/scripts.js
+++ MiniMarketplace/scripts.js
@@ -6,9 +6,19 @@ const submitBtn = document.querySelector("#add-product")
 const container = document.querySelector(".container")
 
 // submit product event
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
 
-// make new product
+    // make new product
+    // console.log('clicked')
+    const product = document.createElement('p')
+    product.innerHTML = `${nameInput.value} - $${priceInput.value}`
 
-// add product to list
+    // add product to list
+    list.appendChild(product)
 
-// clear input
+    // clear input
+    nameInput.value = ""
+})
+
+// future functionality -> error catchs and delete funcionality

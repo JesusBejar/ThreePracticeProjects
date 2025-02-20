e048975b8602a7296208fbd1d52693f880240cba
Jesus Bejar <israelbejarii@gmail.com>
2025-02-19 19:46:54 -0700

    got elements

--- MiniMarketplace/scripts.js
+++ MiniMarketplace/scripts.js
@@ -1,4 +1,9 @@
 // get elements
+const list = document.querySelector("#product-list")
+const nameInput = document.querySelector("#product-name")
+const priceInput = document.querySelector("#product-price")
+const submitBtn = document.querySelector("#add-product")
+const container = document.querySelector(".container")
 
 // submit product event
 
